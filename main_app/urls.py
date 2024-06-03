7f583c64b910beddbf5292c0f96a1c23f35254fa
jsnhn <jayhh122@gmail.com>
2024-06-03 08:46:07 -0700

    adds index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = {
     path("", views.home, name='home'), #route for the homepage
     path("about/", views.about, name="about"),
+    path("foods/", views.foods_index, name='index'),
 }
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+foods = [
+  {'name': 'Mcdonalds', 'meals': 'Big Mac', 'review': 'Very average', 'rating': 5},
+  {'name': 'In n Out', 'meals': 'Double Double', 'review': 'Super tasty', 'rating': 10},
+]
+
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def about(request):
     return render(request, "about.html")
+
+def foods_index(request):
+    return render(request, 'foods/index.html', {
+        'foods': foods
+    })
