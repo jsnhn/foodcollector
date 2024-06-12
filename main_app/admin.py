c35d8185ef3a90d3e6f87c482b83645ab397ce79
jsnhn <jayhh122@gmail.com>
2024-06-11 17:24:58 -0700

    adds Review to admin model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food, Review
 
 # Register your models here.
 admin.site.register(Food)
+admin.site.register(Review)
--- main_app/models.py
+++ main_app/models.py
@@ -6,14 +6,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Food(models.Model):
     name = models.CharField(max_length=100)
     cuisine = models.CharField(max_length=100)
-    rating = models.DecimalField(
-        max_digits=3,
-        decimal_places=1,
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(10)
-        ]
-    ) 
     vegetarian = models.BooleanField(default=False)
 
     def __str__(self):
@@ -23,3 +15,19 @@ class Food(models.Model):
         return reverse('detail', kwargs={'food_id': self.id})
         # Think of the reverse() function as the code equivalent to the url template tag. It returns the correct path for the detail
 
+class Review(models.Model):
+    date = models.DateField(auto_now_add=True)
+    text = models.CharField(max_length=500)
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10)
+        ]
+    ) 
+
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.rating} on {self.date}"
